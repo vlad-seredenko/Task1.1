4db1a0108fc5c74ccd66f7b6b14c898ddbfb9f4a
vlad-seredenko <78535638+vlad-seredenko@users.noreply.github.com>
2021-05-20 23:49:25 +0300

    Add files via upload

---
+++ Task 1.1/Task 1.1/Task 1.1.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+
+using namespace std;
+const float e = 0.0001;
+
+int main(int argc, char** argv)
+{
+    setlocale(LC_ALL, "Ukrainian");
+    float y;
+    float x;
+    cout << "Введiть х = ";
+    cin >> x;
+    y = x;
+    do 
+    {
+        y = (1.0/3.0)*((2*y)+(x/y));
+    }     
+    while (fabs(pow(y, 3) - x) < e);
+    cout << "y = " << y << endl;
+    return 0;
+}
